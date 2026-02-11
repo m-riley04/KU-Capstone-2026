12da5531da4a1477a488d68b24f886aa01d02397
Riley Meyerkorth <117927480+m-riley04@users.noreply.github.com>
2026-02-10 20:17:53 -0600

    add more skeleton stuff

--- rpi/README.md
+++ rpi/README.md
@@ -1,3 +1,6 @@
 # rpi
 
 This directory contains all source code for the Raspberry Pi's boot.
+
+## Raspberry Pi Setup
+
--- rpi/scripts/startup.sh
+++ rpi/scripts/startup.sh
@@ -2,3 +2,4 @@
 
 # This script will be run on the Raspberry Pi at startup to launch the Polypod application with flutter-pi.
 
+call flutter-pi --release --target lib/main.dart
