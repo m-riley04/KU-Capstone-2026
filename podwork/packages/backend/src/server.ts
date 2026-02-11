aafa21fc17bbc5a4423f7a399876ef8bd13675c8
hrsmith1 <hannahrsmith002@gmail.com>
2026-02-11 10:36:33 -0600

    backend init

---
+++ podwork/packages/backend/src/server.ts
@@ -0,0 +1,23 @@
+
+
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT:Number=3000;
+
+// Allow requests from your frontend
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
+
+// Handling GET / Request
+app.get('/', (req : Request, res: Response) => {
+    res.send('Welcome to typescript backend!');
+})
+
+// Server setup
+app.listen(PORT,() => {
+    console.log('The application is listening '
+    + 'on port http://localhost/'+PORT);
+})
