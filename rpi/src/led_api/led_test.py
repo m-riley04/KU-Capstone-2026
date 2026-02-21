cae5e7d0e0323808694a93930394e7dc6b078725
Riley Meyerkorth <117927480+m-riley04@users.noreply.github.com>
2026-02-20 21:05:13 -0600

    make test more iterable and colors more typed

--- rpi/src/led_api/led_test.py
+++ rpi/src/led_api/led_test.py
@@ -1,10 +1,40 @@
 from RPi import GPIO  # type: ignore
+from enum import Enum
 
 # These pin numbers are based on the board's numbering scheme
 # good website: https://pinout.xyz/
 PIN_LED_RED = 40 # aka GPIO 21
 PIN_LED_GREEN = 38 # aka GPIO 20
 PIN_LED_BLUE = 36 # aka GPIO 16
+
+RGBColor = tuple[bool, bool, bool]  # (red, green, blue)
+
+class LEDColor(Enum):
+    """
+    Represents the different colors that the RGB LED can display.
+    Each color represented by a tuple of booleans (RGB) where True means the LED is on and False means it is off.
+    TODO: Maybe could include digital potentiometer values for brightness control in the future? Could get way more complex colors.
+    """
+    OFF: RGBColor = (False, False, False)
+    WHITE: RGBColor = (True, True, True)
+    RED: RGBColor = (True, False, False)
+    GREEN: RGBColor = (False, True, False)
+    BLUE: RGBColor = (False, False, True)
+    YELLOW: RGBColor = (True, True, False)
+    CYAN: RGBColor = (False, True, True)
+    MAGENTA: RGBColor = (True, False, True)
+
+# a map of color names to their corresponding LEDColor enum values for easy access in the test function
+RGB_COLORS = {
+    "off": LEDColor.OFF
+    , "white": LEDColor.WHITE
+    , "red": LEDColor.RED
+    , "green": LEDColor.GREEN
+    , "blue": LEDColor.BLUE
+    , "yellow": LEDColor.YELLOW
+    , "cyan": LEDColor.CYAN
+    , "magenta": LEDColor.MAGENTA
+    }
 
 def setup():
     """
@@ -15,16 +45,16 @@ def setup():
     GPIO.setup(PIN_LED_BLUE, GPIO.OUT)
     GPIO.setup(PIN_LED_GREEN, GPIO.OUT)
 
-def set_led_color(red: bool, blue: bool, green: bool):
+def set_led_color(color: LEDColor):
     """
     Set the color of the RGB LED by controlling the state of each color pin.
-    :param red: If True, the red LED will be on; otherwise, it will be off.
-    :param blue: If True, the blue LED will be on; otherwise, it will be off.
-    :param green: If True, the green LED will be on; otherwise, it will be off.
+    :param color: The color to set the RGB LED to.
     """
+    red, green, blue = color.value
     GPIO.output(PIN_LED_RED, GPIO.HIGH if red else GPIO.LOW)
-    GPIO.output(PIN_LED_BLUE, GPIO.HIGH if blue else GPIO.LOW)
     GPIO.output(PIN_LED_GREEN, GPIO.HIGH if green else GPIO.LOW)
+    GPIO.output(PIN_LED_BLUE, GPIO.HIGH if blue else GPIO.LOW)
+
 
 def run_led_test():
     """
@@ -32,17 +62,9 @@ def run_led_test():
     """
     print("Starting LED test. Press Ctrl+C to exit.")
     try:
-        while True:
-            print("Setting LED to Red...")
-            set_led_color(red=True, blue=False, green=False)
-            input("Press Enter to continue...")
-
-            print("Setting LED to Blue...")
-            set_led_color(red=False, blue=True, green=False)
-            input("Press Enter to continue...")
-
-            print("Setting LED to Green...")
-            set_led_color(red=False, blue=False, green=True)
+        for name, color in RGB_COLORS.items():
+            print(f"Setting LED to {name}...")
+            set_led_color(color)
             input("Press Enter to continue...")
     except KeyboardInterrupt:
         pass
