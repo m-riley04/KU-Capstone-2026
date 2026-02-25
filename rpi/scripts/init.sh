55181b0fa2e59d95a0147dc6e0c9d7f1df8cad59
Riley Meyerkorth <117927480+m-riley04@users.noreply.github.com>
2026-02-24 19:13:13 -0600

    rename the init folder and move the main init script out

--- rpi/scripts/init.sh
+++ rpi/scripts/init.sh
@@ -4,16 +4,17 @@
 # Ideally, this should only be run once.
 
 SCRIPTS_DIR=$(dirname "$0") # gets the current path to the scripts directory
+INIT_SCRIPTS_DIR="$SCRIPTS_DIR/initializations"
 
 # Iterate through all initialization scripts in the scripts directory and run them
 # TODO: maybe this isn't the best approach. Splitting the `source` calls into their own lines instead of using a loop might be more 
 # readable and easier to debug if something goes wrong. We could also add permanent env variables to indicate which scripts have already been run.
 # For now though, this is more concise and scalable.
-for i in "$SCRIPTS_DIR/init_"*.sh; do
+for i in "$INIT_SCRIPTS_DIR"/init_*.sh; do
     if [ -f "$i" ]; then
         echo "Running initialization script: $i"
         source "$i" # source the script to run it in the current shell
     else
-        echo "No initialization scripts found in $SCRIPTS_DIR"
+        echo "No initialization scripts found in $INIT_SCRIPTS_DIR"
     fi
 done
