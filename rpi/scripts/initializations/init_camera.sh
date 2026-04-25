99c16f37c29ac037db55752258cb2ef580c0f772
Riley Meyerkorth <117927480+m-riley04@users.noreply.github.com>
2026-04-25 11:34:21 -0500

    add init script for camera deps

---
+++ rpi/scripts/initializations/init_camera.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Sets up the camera for use with MediaMTX
+
+# Install dependencies
+sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt-get install -y libcamera-apps rpicam-apps
+
